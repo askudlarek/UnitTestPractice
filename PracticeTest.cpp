e182ea26b596bf2af85eb0d0cb902316b46e2991
Adam Skudlarek <skudthe20@gmail.com>
2020-04-23 12:43:11 -0700

    Fix warning, add Unit Tests for Descending

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -63,3 +63,79 @@ TEST(PracticeTest, is_empty_palindrome)
     ASSERT_FALSE(actual);
 }
 
+TEST(PracticeTest, decending_random_order)
+{
+    // Expected order 5 3 1
+    int a = 1;
+    int b = 5;
+    int c = 3;
+    Practice obj;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 5, b == 3, c == 1);
+}
+
+TEST(PracticeTest, decending_opposite_order)
+{
+    // Expected order 332 321 141
+    int a = 141;
+    int b = 321;
+    int c = 332;
+    Practice obj;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 332, b == 321, c == 141);
+}
+
+TEST(PracticeTest, decending_in_order)
+{
+    // Expected order 12 6 2
+    int a = 12;
+    int b = 6;
+    int c = 2;
+    Practice obj;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 12, b == 6, c == 2);
+}
+
+TEST(PracticeTest, decending_negative_numbers)
+{
+    // Expected order -2 -23 -42
+    int a = -23;
+    int b = -42;
+    int c = -2;
+    Practice obj;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == -2, b == -23, c == -42);
+}
+
+TEST(PracticeTest, decending_negative_and_nonnegative)
+{
+    // Expected order 23 -23 -42
+    int a = -23;
+    int b = 23;
+    int c = -42;
+    Practice obj;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 23, b == -23, c == -42);
+}
+
+TEST(PracticeTest, decending_numbers_with_tie)
+{
+    // Expected order 5 5 2 
+    int a = 2;
+    int b = 5;
+    int c = 5;
+    Practice obj;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 5, b == 5, c == 2);
+}
+
+TEST(PracticeTest, decending_numbers_all_tied)
+{
+    // Expected order 34 34 34
+    int a = 34;
+    int b = 34;
+    int c = 34;
+    Practice obj;
+    obj.sortDescending(a, b, c);
+    ASSERT_TRUE(a == 34, b == 34, c == 34);
+}
